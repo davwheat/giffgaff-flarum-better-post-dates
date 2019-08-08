05cc666ab330f6654d9c6a1da3c0136e50ec3432
David Wheatley <blokmaster.help@gmail.com>
2019-08-09 00:45:35 +0100

    Update script

--- flarum-date-fix.user.js
+++ flarum-date-fix.user.js
@@ -3,18 +3,48 @@
 // @description    Some people don't like the 47 minutes ago, 2 hours ago, a day ago styling of dates on the new forum, so I made something to fix that!
 // @author         David Wheatley <davidwheatley03@gmail.com> (https://github.com/davwheat/giffgaff-flarum-better-post-dates)
 // @namespace      https://github.com/davwheat/giffgaff-flarum-better-post-dates
-// @version        1.3.1
-// @icon           https://github.com/davwheat/giffgaff-flarum-better-post-dates/blob/master/giffgaff-flarum-better-post-dates/icon.png?raw=true
+// @version        1.5.0
+// @icon           https://github.com/davwheat/giffgaff-flarum-better-post-dates/blob/master/icon.png?raw=true
 // @match          *://preview.community.giffgaff.com/*
 // @grant          none
 // @run-at         document-end
-// @require        https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js
 // ==/UserScript==
 
-(function($) {
-  "use strict";
+let HL;
+setInterval(() => {
+    if (HL == history.length) return;
 
-  setTimeout(() => {
+    setTimeout(() => {
+        let twoDaysAgoDate = new Date();
+        twoDaysAgoDate.setDate(new Date().getDate() - 1);
+        twoDaysAgoDate.setHours(0);
+        twoDaysAgoDate.setMinutes(0);
+        twoDaysAgoDate.setMilliseconds(0);
+
+        $(".Post .PostMeta time[data-humantime=true]").each((_, el) => {
+            let $te = $(el);
+            let dt = new Date($te.attr("datetime"));
+
+            if (dt > twoDaysAgoDate) {
+                let today = new Date();
+                today.setHours(0);
+                today.setMinutes(0);
+                today.setMilliseconds(0);
+                $te.html(
+                    `${dt > today ? "Today" : "Yesterday"} at ${dt.toLocaleTimeString(
+                        "en-GB",
+                        { hour: "2-digit", minute: "2-digit" }
+                    )}`
+                );
+            }
+        });
+        console.log("Reapplied 'fixed' dates. (This is done to ");
+
+        HL = history.length;
+    }, 750);
+}, 500);
+
+$(() => {
     let twoDaysAgoDate = new Date();
     twoDaysAgoDate.setDate(new Date().getDate() - 1);
     twoDaysAgoDate.setHours(0);
@@ -22,23 +52,21 @@
     twoDaysAgoDate.setMilliseconds(0);
 
     $(".Post .PostMeta time[data-humantime=true]").each((_, el) => {
-      let $te = $(el);
-      let dt = new Date($te.attr("datetime"));
+        let $te = $(el);
+        let dt = new Date($te.attr("datetime"));
 
-      if (dt > twoDaysAgoDate) {
-        let today = new Date();
-        today.setHours(0);
-        today.setMinutes(0);
-        today.setMilliseconds(0);
-        $te.html(
-          `${dt > today ? "Today" : "Yesterday"} at ${dt.toLocaleTimeString(
-            "en-GB",
-            { hour: "2-digit", minute: "2-digit" }
-          )}`
-        );
-      }
+        if (dt > twoDaysAgoDate) {
+            let today = new Date();
+            today.setHours(0);
+            today.setMinutes(0);
+            today.setMilliseconds(0);
+            $te.html(
+                `${dt > today ? "Today" : "Yesterday"} at ${dt.toLocaleTimeString(
+                    "en-GB",
+                    { hour: "2-digit", minute: "2-digit" }
+                )}`
+            );
+        }
     });
-  }, 500);
-}.bind(this)(jQuery));
-
-jQuery.noConflict();
+    console.log("Applied 'fixed' dates.");
+});
