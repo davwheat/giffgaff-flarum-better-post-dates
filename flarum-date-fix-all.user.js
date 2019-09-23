963ed0b5aabc96aafee3338ff4d6d7f0c6994565
David Wheatley <blokmaster.help@gmail.com>
2019-09-23 21:29:39 +0100

    Fix wrongly named variable

--- flarum-date-fix-all.user.js
+++ flarum-date-fix-all.user.js
@@ -3,7 +3,7 @@
 // @description    Some people don't like the 47 minutes ago, 2 hours ago, a day ago styling of dates on the new forum, so I made something to fix that!
 // @author         David Wheatley <davidwheatley03@gmail.com> (https://github.com/davwheat/giffgaff-flarum-better-post-dates)
 // @namespace      https://github.com/davwheat/giffgaff-flarum-better-post-dates
-// @version        2.0.0
+// @version        2.1.0
 // @icon           https://github.com/davwheat/giffgaff-flarum-better-post-dates/blob/master/icon.png?raw=true
 // @match          *://community.giffgaff.com/*
 // @grant          none
@@ -12,7 +12,7 @@
 
 setInterval(() => {
     $(".Post .PostMeta time[data-humantime=true]").each((_, el) => {
-        let d = new Date($te.attr("datetime"));
+        let d = new Date($(el).attr("datetime"));
 
         $(el).html(`${d.toLocaleDateString()} at ${d.toLocaleTimeString()}`);
     });
@@ -22,7 +22,7 @@ setInterval(() => {
 
 $(() => {
     $(".Post .PostMeta time[data-humantime=true]").each((_, el) => {
-        let d = new Date($te.attr("datetime"));
+        let d = new Date($(el).attr("datetime"));
 
         $(el).html(`${d.toLocaleDateString()} at ${d.toLocaleTimeString()}`);
     });
